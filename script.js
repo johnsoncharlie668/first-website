b453537f68eaace4b9768876bd1683a5431e7615
Charles Johnson <charliejohnson@Mac.home>
2025-08-07 13:53:07 -0400

    Mapped out website layout with CSS and HTML. Begun to add js functionality

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
-const listItems = document.querySelectorAll("li");
+const books = [
+    {
+        title: 'Frankenstein',
+        author: 'Mary Shelley',
+        coverImage: 'images/book-covers/frankenstein-cover.jpg',
+        style: 4.5,
+        story: 4.5,
+        soul: 5,
+    },
+    {
+        title: 'The Stranger',
+        author: 'Albert Camus',
+        coverImage: 'images/book-covers/the-stranger-cover.jpg',
+        style: 5,
+        story: 4,
+        soul: 5,
+    },
+    {
+        title: 'The Metamorphosis',
+        author: 'Franz Kafka',
+        coverImage: 'images/book-covers/the-metamorphosis-cover.jpg',
+        style: 5,
+        story: 4,
+        soul: 4.5,
+    },
+];
 
-function toggleDone(event) {
-    if (!event.target.className) {
-        event.target.className = "done";
-    }
-    else {
-        event.target.className = ""
-    }
+// Calculate overall rating for each book
+for (let book in books) {
+    book.rating = Math.round((book.style + book.story + book.soul / 3));
 }
 
-listItems.forEach((item) => {
-    item.addEventListener("click", toggleDone)
-});
+// Set starting conditions
+currentBookIndex = 1;
+
